70ba5abec521a7ea9aadfeedcafb6bd35d539cd4
Max <hellhunter15@hotmail.com>
2021-10-16 03:54:05 +0200

    Add:  csv file, User model, TODO's;

---
+++ CSVPaginatedApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVPaginatedApp.Models
+{
+    public class User
+    {
+        //id,first_name,last_name,birth_date,salary
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Decimal Salary { get; set; }
+    }
+}
--- CSVPaginatedApp/Program.cs
+++ CSVPaginatedApp/Program.cs
@@ -6,6 +6,10 @@ namespace CSVPaginatedApp
     {
         static void Main(string[] args)
         {
+            //TODO Read Users from File.
+            //TODO Paginate returns.
+            //TODO EXTRA: Filters. Age and Salary
+            //TODO EXTRA OrderBy lastname and firstname Salary Age
             Console.WriteLine("Hello World!");
         }
     }
